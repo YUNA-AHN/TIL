ddf9a3ee32bb899c74f8ed796e2221e3b03decfc
DESKTOP-DR1CL77\sgvin <yunanash1234@gmail.com>
2023-08-07 01:11:03 +0900

    snail..

--- 02_Algorithm/04_List2/04_DAY_달팽이.py
+++ 02_Algorithm/04_List2/04_DAY_달팽이.py
@@ -15,5 +15,31 @@ for tc in range(1, T+1):
     di = [0, -1, 0, 1]
     dj = [1, 0, -1, 0]
 
+    # N * N 설정
     arr = [[0] * N for _ in range(N)]
 
+    cnt = 1 #카운트
+    # 좌표값과 방향값
+    r_idx = c_idx = i = 0
+    # 초기값 설정
+    arr[r_idx][c_idx] = cnt
+    while cnt < N * N:
+        # 델타값으로 진행
+        ni = r_idx + di[i]
+        nj = c_idx + dj[i]
+
+        # r_idx 나 c_idx가 끝에 도달했거나, 이미 입력된 값과 마주한다면
+        if 0 > ni or ni >= N or 0 > nj or nj >= N or arr[ni][nj] != 0:
+            i = (i + 1) % 4
+            continue
+
+        # 정상적으로 카운트 값을 넣어햐 하는 경우에는
+        r_idx += di[i]
+        c_idx += dj[i]
+        cnt += 1
+        arr[r_idx][c_idx] = cnt
+
+    print(f'#{tc}')
+
+    for item in arr:
+        print(*item)
