82acc61a08224c19fbf7f08a117076ed45e7cb89
DESKTOP-DR1CL77\sgvin <yunanash1234@gmail.com>
2023-08-30 01:04:15 +0900

    sadness...

--- boj 2529.py
+++ boj 2529.py
@@ -9,26 +9,49 @@ import sys
 
 n = int(sys.stdin.readline())
 arr = sys.stdin.readline().split()
+total = []
 
 for i in range(10):
     visited = [0] * 10
     lst = [i] # 첫번째 값은 일단 순차적으로 돌기
     visited[i] = 1
-    for es in arr:
-        if es == '>':
+    l = 0 # 부등호의 인덱스
+    while l < len(arr): # 부등호 길이보다 작은 동안에
+        if arr[l] == '>':
             for j in range(10):
                 if j < lst[-1] and visited[j] == 0:
                     visited[j] = 1
                     lst.append(j)
                     break
-                else:
-                    continue
         else:
             for k in range(10):
                 if k > lst[-1] and visited[k] == 0:
                     visited[k] = 1
                     lst.append(k)
                     break
-                else:
-                    continue
+        if l + 1 <= len(arr)  and len(lst)-1 != l + 1:
+            # 여기서의 문제점 원인 인덱스로 돌아가지 않음..
+            visited[lst[-1]] = 0
+            lst[-1] += 1
+            if lst[-1] <= 9:
+                visited[lst[-1]] = 1
+            else:
+                break
+        else:
+            l += 1
+
     print(lst)
+
+#     total.append(''.join(map(str, lst)))
+#
+# mx = 0
+# mn = 10000000000
+#
+# for i in total:
+#     mx = max(mx, int(i))
+#     mn = min(mn, int(i))
+#
+# print(mx, mn)
+
+
+
